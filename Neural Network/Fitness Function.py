6d2b4638dc4268290daea606fe745effae41ea33
JayDigvijay <40368280+JayDigvijay@users.noreply.github.com>
2020-11-03 00:32:35 +0530

    Fitness Function code modified

--- Neural Network/Fitness Function.py
+++ Neural Network/Fitness Function.py
@@ -6,7 +6,8 @@ Created on Mon Nov  2 22:44:37 2020
 """
 import pandas as pd
 from ANN import evaluate_algorithm, load_csv, dataset_minmax, normalize_dataset, back_propagation, str_column_to_int, str_column_to_float
-def Fitness(gene, n_folds, l_rate, n_epoch, n_hidden):
+
+def Fitness(gene):
     sequence = list()
     for x in gene:
         sequence.append(int(x))
@@ -29,12 +30,12 @@ def Fitness(gene, n_folds, l_rate, n_epoch, n_hidden):
     minmax = dataset_minmax(dataset)
     normalize_dataset(dataset, minmax)
     # evaluate algorithm
-    '''
+    
     n_folds = 3
     l_rate = 0.3
     n_epoch = 200
     n_hidden = 5
-    '''
+    
     scores = evaluate_algorithm(dataset, back_propagation, n_folds, l_rate, n_epoch, n_hidden)
     print('Scores: %s' % scores)
     print('Mean Accuracy: %.3f%%' % (sum(scores)/float(len(scores))))
